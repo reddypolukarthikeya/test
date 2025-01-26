c3073c5b8c02dd40bcd5aa4df5c6e8f3b91dcc5d
EC2 Default User <ec2-user@ip-172-31-91-137.ec2.internal>
2025-01-26 17:06:35 +0000

    added password reset api

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 import { HttpError } from "../models/error.js";  
 import { User } from "../models/user.js";        
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 // User Registration Controller
 const register = async (req, res, next) => {
@@ -23,7 +23,7 @@ const register = async (req, res, next) => {
       return next(new HttpError("Passwords do not match", 422));
     }
 
-    const salt = await bcrypt.genSalt(11);  // 11 rounds are good and protects against attacks
+    const salt = await bcrypt.genSalt(11);  // 11 rounds are good and protect against attacks
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = await User.create({ name, email: newEmail, password: hashedPassword, role });
     res.status(201).json({ newUser });
@@ -31,44 +31,69 @@ const register = async (req, res, next) => {
     return next(new HttpError("User Registration Failed", 422));
   }
 };
-const login = async(req,res,next) => {
-  try {
-    const {email,password} = req.body;
-    if(!email || !password) {
-      return next(new HttpError("Fill in all fields", 422));
 
+// User Login Controller
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(new HttpError("Fill in all fields", 422));
     }
     const newEmail = email.toLowerCase();
-    const user = await User.findOne({email: newEmail});
-    if(!user) {
+    const user = await User.findOne({ email: newEmail });
+    if (!user) {
       return next(new HttpError("Invalid Credentials", 422));
     }
-    const comparePassword = await bcrypt.compare(password,user.password);
-    // user.password is the hashed password from database
-    if(!comparePassword) {
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
       return next(new HttpError("Invalid Password", 422));
     }
-    const {_id: id, name, role} = user;
-    // a token requires a payload to work
-     const token = jwt.sign({id, name, role}, process.env.JWT_SECRET, {expiresIn: "2d"});
-     res.status(200).json({token,id,name, role})
-
-  } catch(error) {
+    const { _id: id, name, role } = user;
+    const token = jwt.sign({ id, name, role }, process.env.JWT_SECRET, { expiresIn: "2d" });
+    res.status(200).json({ token, id, name, role });
+  } catch (error) {
     return next(new HttpError(error));
   }
-}
-const getUserProfile = async(req,res,next) => {
-  try {
-   // extracting the id
-    const {id} = req.params;
-    const user = await User.findById(id).select('-password'); // don't send the password back
-    if(!user) {
-     return next(new HttpError("User not found", 422));
-    }
-res.status(200).json(user);
-} catch(error) {
- return next(new HttpError(error));
-  }
-}
+};
 
-export { register, login , getUserProfile};  
+// Get User Profile Controller
+const getUserProfile = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).select("-password"); // Exclude password from response
+    if (!user) {
+      return next(new HttpError("User not found", 422));
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    return next(new HttpError(error));
+  }
+};
+
+// Reset Password Controller
+const resetPassword = async (req, res, next) => {
+  try {
+    const { email, password, confirmPassword } = req.body;
+    if (!email || !password || !confirmPassword) {
+      return next(new HttpError("Fill in all fields", 422));
+    }
+    if (password !== confirmPassword) {
+      return next(new HttpError("Passwords do not match", 422));
+    }
+    const user = await User.findOne({ email: email.toLowerCase() });
+    if (!user) {
+      return next(new HttpError("User with this email does not exist", 404));
+    }
+    const salt = await bcrypt.genSalt(11);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    user.password = hashedPassword;
+    await user.save();
+    res.status(200).json({ message: "Password reset successfully" });
+  } catch (error) {
+    return next(new HttpError(error));
+  }
+};
+
+// Export all controllers
+export { register, login, getUserProfile, resetPassword };
+
